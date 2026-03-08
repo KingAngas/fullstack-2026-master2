09018d9f0ce6f7dacbfebcedd59688c28c2433e3
KingAngas <sagnamouhamed0@gmail.com>
2026-03-08 20:48:42 +0000

    base de donnee

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -5,20 +5,20 @@ import * as  store from "../store/students.store.js"
 const router = express.Router();
 
 //Endpoints
-router.get("/",(_, res) => {
-    const items = store.getAll({page: 1 , limit: 100}).data ; 
+router.get("/",async(_, res) => {
+    const items = await store.getAll({page: 1 , limit: 100}) ; 
     //res.json(items);
      res.render("students/index", { items});
 })
 
 
-router.get("/api",(_, res) => {
-    const items = store.getAll({page: 1 , limit: 100}).data ; 
+router.get("/api",async(_, res) => {
+    const items = await store.getAll({page: 1 , limit: 100}) ; 
     res.json(items);
 })
 
-router.post("/api",(req, res) => {
-    const created = store.create(req.body);
+router.post("/api",async(req, res) => {
+    const created =await store.create(req.body);
     res.status(201).json(created);
 })
 
--- store/students.store.js
+++ store/students.store.js
@@ -1,23 +1,24 @@
 import { randomUUID} from 'crypto' ;
+import  pool from '../db.js' ;
+export const getAll = async (query = {}) => {
+  const limitInt  = Number(query.limit ?? 100);
+  const offsetInt = Number(((query.page ?? 1) - 1) * limitInt);
+  const [[{ total }]] = await pool.execute(
+    "SELECT COUNT(*) AS total FROM students"
+  );
+const sqlQuery = `SELECT * FROM students ORDER BY createdAt DESC LIMIT ${limitInt} OFFSET ${offsetInt}` ;
+  const [rows] = await pool.query(sqlQuery);
+  return {
+    data: rows,
+    total,
+    page: query.page ?? 1,
+    limit: limitInt,
+  };
+};
 
-
-let items = [] ;
-
-// recuper la liste des etudiants en mode paginee
-export const getAll = ({page =1 , limit=10}) =>{
-    const start = (page -1 ) * limit ;
-    const end  = start  + limit ; 
-
-    return {
-        data : items.slice(start,end),
-        total : items.length,
-        page, 
-        limit
-    }
-}
 
 // Ajout d'un étudiant
-export const create = (payload) => {
+/* export const create = (payload) => {
    const newItem = {
         id : randomUUID,
         ...payload,
@@ -29,3 +30,4 @@ export const create = (payload) => {
 } ;
 
 
+ */
